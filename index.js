233a918d5d0dc3fc2fa4d958f1d8391aeeee77a4
Saikiran <raosaikiran02@gmail.com>
2023-06-27 14:37:47 +0530

    ejs rendering set

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const { connectToMongoDB } = require("./connection");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
@@ -7,15 +8,24 @@ const URL = require("./models/url");
 const app = express();
 const PORT = 8000 || process.env.PORT;
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected")
 );
 
 app.use(express.json());
 
+app.get("/test", async(req,res) => {
+  const allUrls = await URL.find({});
+  return res.render ('home')
+
+});
+
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
